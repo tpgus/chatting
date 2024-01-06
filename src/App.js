a285fb747fedeffa315ca1895e62f726afe3f48c
Sehyun <sehyun.kim@themobilityone.com>
2024-01-06 21:55:48 +0900

    feat: 페이지 라우팅 및 레이아웃 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home';
 import Layout from './components/layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
